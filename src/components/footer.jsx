51ee329acb89e42a02818370fbc99153d933d039
Ansh Chauhan <achauhan4580@gmail.com>
2021-09-29 09:06:41 +0530

    date update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <div className="footer">
             <h1 className="head">Theme JSX</h1>
-            <h2>Last Updated: <code>2nd June 2021</code></h2>
+            <h2>Last Updated: <code>29 September 2021</code></h2>
         </div>
     )
 }
